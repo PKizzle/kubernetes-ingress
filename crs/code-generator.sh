d9a78f1835da5b7cb080ace4a0277afe269c9253
Philipp Kolberg <philipp.kolberg@t-online.de>
2024-10-26 00:17:58 +0200

    Refactor code generator script to handle sed command syntax differences on macOS and other operating systems

--- crs/code-generator.sh
+++ crs/code-generator.sh
@@ -9,8 +9,13 @@ client_native_version=$(go list -m -f "{{.Version}}" github.com/haproxytech/clie
 echo "Client Native Version: $client_native_version"
 for file in crs/api/ingress/v1/*.go; do
     echo "$file"
-    # Use sed to replace the version string in Go files with the new version
-    sed -i  "s@// +kubebuilder:metadata:annotations=\"haproxy.org/client-native=.*\"@// +kubebuilder:metadata:annotations=\"haproxy.org/client-native=$client_native_version\"@" $file
+	# Use sed to replace the version string in Go files with the new version
+	# Check on which os the script is running and use the correct sed command (macOS uses a different syntax)
+	if [[ "$OSTYPE" == "darwin"* ]]; then
+		gsed -i "s@// +kubebuilder:metadata:annotations=\"haproxy.org/client-native=.*\"@// +kubebuilder:metadata:annotations=\"haproxy.org/client-native=$client_native_version\"@" $file
+    else
+    	sed -i "s@// +kubebuilder:metadata:annotations=\"haproxy.org/client-native=.*\"@// +kubebuilder:metadata:annotations=\"haproxy.org/client-native=$client_native_version\"@" $file
+	fi
 done
 
 # code-generator build native, versioned clients, informers and other helpers
@@ -20,8 +25,11 @@ CR_DIR=$( cd -- "$( dirname -- "$( readlink -f -- "${BASH_SOURCE[0]}"; )" )" &> 
 OUTPUT_DIR="${CR_DIR}/.generated"
 HDR_FILE="$( readlink -f -- "${CR_DIR}/../assets/license-header.txt"; )"
 CR_PKG="github.com/haproxytech/kubernetes-ingress/crs"
-API_PKGS=$(find ${CR_DIR}/api -mindepth 2 -type d -printf "$CR_PKG/api/%P\n"| sort | tr '\n' ',')
-API_PKGS=${API_PKGS::-1} # remove trailing ","
+if [[ "$OSTYPE" == "darwin"* ]]; then
+	API_PKGS=$(find "${CR_DIR}/api" -mindepth 2 -type d | gsed "s|^${CR_DIR}/api/|${CR_PKG}/api/|" | sort | tr '\n' ',' | gsed 's/,$//')
+else
+	API_PKGS=$(find "${CR_DIR}/api" -mindepth 2 -type d | sed "s|^${CR_DIR}/api/|${CR_PKG}/api/|" | sort | tr '\n' ',' | sed 's/,$//')
+fi
 
 # Install Kubernetes Code Generators from k8s.io/code-generator
 
@@ -39,53 +47,53 @@ echo "Generating code for $API_PKGS"
 echo "Generating deepcopy funcs"
 GOPATH=$gopath "${gobin}/deepcopy-gen"\
   -O zz_generated.deepcopy\
-	--input-dirs ${API_PKGS}\
-	--go-header-file ${HDR_FILE}\
-	--output-base ${OUTPUT_DIR}
+	--input-dirs "${API_PKGS}"\
+	--go-header-file "${HDR_FILE}"\
+	--output-base "${OUTPUT_DIR}"
 
 echo "Generating register funcs"
 GOPATH=$gopath "${gobin}/register-gen"\
   -O zz_generated.register\
-	--input-dirs ${API_PKGS}\
-	--go-header-file ${HDR_FILE}\
-	--output-base ${OUTPUT_DIR}
+	--input-dirs "${API_PKGS}"\
+	--go-header-file "${HDR_FILE}"\
+	--output-base "${OUTPUT_DIR}"
 
 echo "Generating clientset"
 GOPATH=$gopath "${gobin}/client-gen"\
 	--plural-exceptions "Defaults:Defaults"\
 	--clientset-name "versioned"\
-	--input ${API_PKGS}\
+	--input "${API_PKGS}"\
 	--input-base "" \
 	--output-package "${CR_PKG}/generated/clientset"\
-	--go-header-file ${HDR_FILE}\
-	--output-base ${OUTPUT_DIR}
+	--go-header-file "${HDR_FILE}"\
+	--output-base "${OUTPUT_DIR}"
 
 echo "Generating listers"
 GOPATH=$gopath "${gobin}/lister-gen"\
 	--plural-exceptions "Defaults:Defaults"\
-	--input-dirs ${API_PKGS}\
+	--input-dirs "${API_PKGS}"\
 	--output-package "${CR_PKG}/generated/listers"\
-	--go-header-file ${HDR_FILE}\
-	--output-base ${OUTPUT_DIR}
+	--go-header-file "${HDR_FILE}"\
+	--output-base "${OUTPUT_DIR}"
 
 echo "Generating informers"
 GOPATH=$gopath "${gobin}/informer-gen"\
 	--plural-exceptions "Defaults:Defaults"\
-	--input-dirs ${API_PKGS}\
+	--input-dirs "${API_PKGS}"\
 	--versioned-clientset-package "${CR_PKG}/generated/clientset/versioned"\
 	--listers-package "${CR_PKG}/generated/listers"\
 	--output-package "${CR_PKG}/generated/informers"\
-	--go-header-file ${HDR_FILE}\
-	--output-base ${OUTPUT_DIR}
+	--go-header-file "${HDR_FILE}"\
+	--output-base "${OUTPUT_DIR}"
 
 # Copy generated code into the right location
 # This extra step is required because code generator seems to be working only in a GOPATH environment.
 # https://github.com/kubernetes/code-generator/issues/57
 # So the work around is to generate to OUTPUT_DIR then move code to the right location
-find  ${OUTPUT_DIR}/${CR_PKG}/api -mindepth 2 -type f -printf "%P\n" | xargs -I{} cp ${OUTPUT_DIR}/${CR_PKG}/api/{} ${CR_DIR}/api/{}
-mkdir -p ${CR_DIR}/generated
-cp -r ${OUTPUT_DIR}/${CR_PKG}/generated/{clientset,listers,informers} ${CR_DIR}/generated
-rm -rf ${OUTPUT_DIR}
+find "${OUTPUT_DIR}/${CR_PKG}/api" -mindepth 2 -type f | sed 's|^'"${OUTPUT_DIR}/${CR_PKG}/api/"'||' | xargs -I{} cp "${OUTPUT_DIR}/${CR_PKG}/api/{}" "${CR_DIR}/api/{}"
+mkdir -p "${CR_DIR}/generated"
+cp -r "${OUTPUT_DIR}/${CR_PKG}/generated/{clientset,listers,informers}" "${CR_DIR}/generated"
+rm -rf "${OUTPUT_DIR}"
 
 CONTROLLER_GEN_VERSION=$(go list -m  sigs.k8s.io/controller-tools  | cut -d ' ' -f2)
 go install sigs.k8s.io/controller-tools/cmd/controller-gen@${CONTROLLER_GEN_VERSION}
